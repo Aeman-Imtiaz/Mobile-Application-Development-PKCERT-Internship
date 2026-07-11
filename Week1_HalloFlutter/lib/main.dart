bcb000372fdb8bdb7a5e88cb669a4428d644f748
Aeman-Imtiaz <khaanaeman@gmail.com>
2026-07-11 17:38:27 +0500

    Week 1: Flutter environment setup and first project

---
+++ Week1_HalloFlutter/lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: ' My Information'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: .center,
+          children: [
+            const Text(
+              'Aeman Imtiaz',
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+            ),
+            const Text(
+              'Reg No: 04162313001',
+              style: TextStyle(fontSize: 18),
+            ),
+            const SizedBox(height: 30),
+            const Text('Quaid-iAzam University, Islamabad  '),
+          ],
+        ),
+      ),
+    );
+  }
+}
